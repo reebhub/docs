b63bd8579386c781ce5041e0ea4d38f22a2e67e9
reebhub <reebreeb@gmail.com>
2020-04-23 13:34:24 +0300

    RDoc-1703_TimeSeries_5.0 Time Series documentation skeleton

---
+++ Raven.Documentation.Parser/Data/Category.cs
@@ -0,0 +1,81 @@
+namespace Raven.Documentation.Parser.Data
+{
+	using System.ComponentModel;
+
+	using Raven.Documentation.Parser.Attributes;
+
+	public enum Category
+	{
+		[Prefix("client-api")]
+		[Description("Client API")]
+		ClientApi,
+
+		[Prefix("server")]
+		Server,
+        
+        [Prefix("database")]
+        Database,
+
+		[Prefix("studio")]
+		Studio,
+
+		[Prefix("transformers")]
+		Transformers,
+
+		[Prefix("indexes")]
+		Indexes,
+
+		[Prefix("glossary")]
+		Glossary,
+
+		[Prefix("start")]
+		[Description("Getting Started")]
+		Start,
+
+		[Prefix("file-system")]
+		[Description("File System")]
+		FileSystem,
+
+		[Prefix("users-issues")]
+		[Description("Users Issues")]
+		UsersIssues,
+
+		// legacy categories
+
+		[Prefix("intro")]
+		Intro,
+
+		[Prefix("theory")]
+		Theory,
+
+		[Prefix("http-api")]
+		[Description("HTTP API")]
+		HttpApi,
+
+		[Prefix("appendixes")]
+		Appendixes,
+
+		[Prefix("faq")]
+		[Description("FAQ")]
+		Faq,
+
+		[Prefix("samples")]
+		Samples,
+
+		[Prefix("index")]
+		Index,
+
+        [Prefix("articles")]
+        Articles,
+
+        [Prefix("migration")]
+        Migration,
+
+        [Prefix("cloud")]
+        Cloud,
+
+        [Prefix("document-extensions")]
+        [Description("Document Extensions")]
+        DocumentExtensions
+    }
+}
